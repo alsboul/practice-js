358e0ebeaed48dcc9c1c4c17c99e5073b938c6e4
mohammad alsboul <mohammad.alspoole@gmail.com>
2019-11-05 17:59:43 +0200

    java-script

--- c01/add-content.js
+++ c01/add-content.js
@@ -1,18 +1,21 @@
-var today = new Date();
-var hourNow = today.getHours();
+// var today = new Date();
+// console.log(today);
+// console.log("amman-102d3");
+// var hourNow = today.getHours();
+// console.log(hourNow);
+
+
+var hourNow = prompt("what time is now?");
 var greeting;
-if (hourNow > 18) {
-    greeting = 'good evening!';
+
+if (hourNow > 18 && hourNow <24 ) {
+  greeting = 'Good evening, Class!';
+} else if (hourNow > 12 && hourNow <=18) {
+  greeting = 'Good afternoon, Class!';
+} else if (hourNow >= 0 && hourNow <=12) {
+  greeting = 'Good morning!';
+} else {
+  greeting = 'Welcome!';
 }
-else if
-    (hourNow > 12) {
-    greeting = 'Good afternoon!';
-}
-else if (
-    hourNow > 0) {
-    greeting = 'Good morning';
-}
-else {
-    greeting = 'Welcome!';
-}
+
 document.write('<h3>' + greeting + '</h3>');
